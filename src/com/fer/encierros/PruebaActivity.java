adb3aaff5c1880ccf8f4e918ded6a5c13483504f
fer <fernando.lopez.martin@gmail.com>
2014-07-27 23:40:23 +0200

    Revert "Revert "Version 0.0.1""
    This reverts commit 7cc60dd36c791ccfc276e6245ce42a806c335989.

---
+++ src/com/fer/encierros/PruebaActivity.java
@@ -0,0 +1,13 @@
+package com.fer.encierros;
+
+import org.apache.cordova.DroidGap;
+import android.os.Bundle;
+
+public class PruebaActivity extends DroidGap {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.loadUrl("file:///android_asset/www/index.html");
+    }
+}
